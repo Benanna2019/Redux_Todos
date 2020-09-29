a9f395aceb5cc352543508ff7af869715f6e134a
Benanna2019 <bass41992ben@gmail.com>
2020-09-29 15:03:59 -0400

    created visibility filter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
+import Footer from "./components/Footer";
 
 function App() {
   const [todos, setTodos] = useState([
     { id: 0, text: "read new Tolkein books I got today", completed: false },
     { id: 1, text: "drink some coffee", completed: true },
   ]);
+  const [visibilityFilter, setVisibilityFilter] = useState("SHOW_ALL");
   console.log(todos);
+  console.log(visibilityFilter);
   return (
     <div>
       <AddTodo setTodos={setTodos} todos={todos} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList
+        visibilityFilter={visibilityFilter}
+        todos={todos}
+        setTodos={setTodos}
+      />
+      <Footer setVisibilityFilter={setVisibilityFilter} />
     </div>
   );
 }
